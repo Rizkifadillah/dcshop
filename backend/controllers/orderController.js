a4430fe940dba5bab1c24c6adc675b207cbc7960
RizkiFadillah <rizkifadillah26.rf@gmail.com>
2022-11-11 21:04:20 +0700

    update status order with admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -57,7 +57,7 @@ exports.myOrders = catchAsyncErrors(async(req,res,next)=>{
     })
 })
 
-// Get all order => /api/v1/admin/orders/
+// Get all order - ADMIN => /api/v1/admin/orders/
 exports.allOrders = catchAsyncErrors(async(req,res,next)=>{
     const orders = await Order.find()
 
@@ -73,3 +73,35 @@ exports.allOrders = catchAsyncErrors(async(req,res,next)=>{
         orders
     })
 })
+
+// Update / Process order - ADMIN => /api/v1/admin/order/:id
+exports.updateOrder = catchAsyncErrors(async(req,res,next)=>{
+    const order = await Order.findById(req.params.id)
+
+    if(order.orderStatus === 'Delivered'){
+        return next(new ErrorHandler('You have already delivered this order', 400))
+    }
+console.log(order.orderStatus)
+order.orderItems.forEach(async item=>{
+    await updateStock(item.product, item.quantity)
+    })
+
+    order.orderStatus = req.body.status,
+    order.deliveredAt = Date.now()
+
+    await order.save()
+    console.log(order.orderStatus)
+    res.status(200).json({
+        success: true,
+        // totalAmount,
+        // order
+    })
+})
+
+async function updateStock(id, quantity){
+    const product = await Product.findById(id);
+
+    product.stock = product.stock - quantity;
+
+    await product.save({ validateBeforeSave: false});
+}
